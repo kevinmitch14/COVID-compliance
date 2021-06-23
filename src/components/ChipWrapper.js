c37eb580488347238b563c09e91ed9ed59a92ecb
Kevin Mitchell <kevinmitch14@gmail.com>
2021-06-23 12:57:59 +0200

    Chip implementation updates

--- src/components/ChipWrapper.js
+++ src/components/ChipWrapper.js
@@ -1,25 +1,25 @@
 import React, { useState } from 'react'
 import Chip from '@material-ui/core/Chip';
+import { onRemove, onSelect } from '../slices/chips';
+import { useDispatch } from 'react-redux';
 
 const ChipWrapper = ({ chipActiveHandler, chipDeleteHandler, id, label }) => {
 
     const [chipActive, setChipActive] = useState(false)
 
+    const dispatch = useDispatch()
 
-    const wrappedChip =
-        <Chip
-
-            label={label}
-            clickable
-            onClick={(e) => { setChipActive(!chipActive); chipActiveHandler(id) }}
-            style={chipActive ? { backgroundColor: '#35b999' } : { backgroundColor: '#04363d', color: 'white', border: '1px solid white' }}
-            onDelete={(e) => { setChipActive(false); chipDeleteHandler(id) }}
-            deleteIcon={'X'}
-
-        />
     return (
         <div>
-            {wrappedChip}
+            <Chip
+                label={label}
+                clickable
+                onClick={() => { dispatch(onSelect(label)); setChipActive(true) }}
+                style={chipActive ? { fontSize: '16px', backgroundColor: '#35b999' } : { fontSize: '16px', backgroundColor: '#04363d', color: 'white', border: '1px solid white' }}
+                onDelete={() => { dispatch(onRemove(label)); setChipActive(false) }}
+                deleteIcon={'X'}
+                size={'medium'}
+            />
         </div>
     )
 }
