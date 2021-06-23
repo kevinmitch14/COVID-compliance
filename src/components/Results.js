a44c62db9a44c27b478cfa685c215a1047444994
Kevin Mitchell <kevinmitch14@gmail.com>
2021-06-23 12:59:05 +0200

    Link with chip choices + filtering functionality

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,77 +1,102 @@
 import Comment from '../components/Comment'
 import { CircularProgress } from '@material-ui/core';
-import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
 
-const Results = ({ placeData, loading, actives }) => {
+const Results = ({ placeData, loading }) => {
 
     const SVGstyle = { display: 'block', margin: 'auto', padding: '50px' }
-    // placeData.forEach(element => {
-    //     console.log(element.extraData.types[0])
-    // });
-    const [activeCats, setActiveCats] = useState([])
+    const chips = useSelector(state => state.chip)
+    const [filtered, setFiltered] = useState([])
+    console.log(chips.activeCats)
 
+    const dict = {
+        'Restaurants': ['restaurant', 'food'],
+        'Hotels': 'lodging',
+        'Landmarks': 'tourist_attraction',
+        'Most Reviewed': 'reviewed',
+        'Bars': 'bar',
+        'Retail': ["furniture_store", "department_store", "supermarket"]
+    }
 
-    useEffect(() => {
-        let newArr = [...activeCats]
-        actives.forEach((item) => item.active === true && !activeCats.includes(item.google_name) ? setActiveCats(prevState => [...prevState, item.google_name]) : console.log("No active"))
-        actives.forEach((item) => {
-            if (item.active === false && activeCats.includes(item.category)) {
-                let deletedChip = item.category
-                newArr.includes(deletedChip) ? newArr = newArr.filter((item) => item !== deletedChip) : console.log(100)
-                setActiveCats(newArr)
-            }
+    const chipRed = useRef(() => { })
+
+    chipRed.current = () => {
+        chips.activeCats.forEach((item) => {
+            let placeType = dict[item]
+            console.log(placeType)
+            // let p = placeData.filter((item) => item.extraData.types[0] === placeType || item.extraData.types[1] === placeType)
+            let p = placeData.filter((item) => placeType.includes(item.extraData.types[0]) || placeType.includes(item.extraData.types[1]))
+
+            setFiltered(prevState => [...prevState, ...p])
         })
-
-    }, [activeCats, actives])
-
-    // placeData.forEach((item) => console.log(item.extraData.types))
-    const [filterItems, setFilterItems] = useState([])
-    console.log(filterItems)
-    console.log(activeCats)
-
+    }
 
     useEffect(() => {
-        let newArr = []
-        activeCats.forEach(element => {
-            let p = element
-            console.log(element)
-            let x = placeData.filter((element) => element.extraData.types[0] === p)
-            setFilterItems((prevState) => [...x])
-            console.log(newArr)
-
-        });
-    }, [activeCats, placeData])
+        chipRed.current()
+        return () => {
+            setFiltered([])
+        }
+    }, [chips.activeCats])
 
 
-    console.log(filterItems)
+    const resultsList = chips.activeCats.length !== 0 ?
+        filtered.sort((a, b) => (a.average < b.average) ? 1 : -1)
+            .map((item, index) => {
+                return (
+                    <div className='result-card' key={index}>
+
+                        <div className="name">
+                            <p id="card-name">{item.place}</p>
+                            <p id="card-county">{item.county}</p>
+                        </div>
+
+                        <div className="rating">
+                            <span id="first">{(Math.floor(Math.round(((item.accumRating / 3) / item.count) * 10) / 10))}.</span><span id="last">{Math.round(((Math.round(((item.accumRating / 3) / item.count) * 10) / 10) - Math.floor(Math.round(((item.accumRating / 3) / item.count) * 10) / 10).toFixed()) * 10)}</span>
+                        </div>
+
+                        <div className="reviews">
+                            {item.count > 1 ? <span>{item.count} reviews</span> : <span>{item.count} review</span>}
+                        </div>
+
+                        <Comment
+                            cleanRating={Math.round(item.cleanRating / item.count * 10) / 10}
+                            staffRating={Math.round(item.staffRating / item.count * 10) / 10}
+                            adheranceRating={Math.round(item.adheranceRating / item.count * 10) / 10}
+                            extraData={item.extraData}
+                        />
+                    </div>
+                )
+            }) :
 
 
-    const resultsList = placeData.map((item, index) => {
-        return (
-            <div className='result-card' key={index}>
 
-                <div className="name">
-                    <p id="card-name">{item.place}</p>
-                    <p id="card-county">{item.county}</p>
+        placeData.map((item, index) => {
+            return (
+                <div className='result-card' key={index}>
+
+                    <div className="name">
+                        <p id="card-name">{item.place}</p>
+                        <p id="card-county">{item.county}</p>
+                    </div>
+
+                    <div className="rating">
+                        <span id="first">{(Math.floor(Math.round(((item.accumRating / 3) / item.count) * 10) / 10))}.</span><span id="last">{Math.round(((Math.round(((item.accumRating / 3) / item.count) * 10) / 10) - Math.floor(Math.round(((item.accumRating / 3) / item.count) * 10) / 10).toFixed()) * 10)}</span>
+                    </div>
+
+                    <div className="reviews">
+                        {item.count > 1 ? <span>{item.count} reviews</span> : <span>{item.count} review</span>}
+                    </div>
+
+                    <Comment
+                        cleanRating={Math.round(item.cleanRating / item.count * 10) / 10}
+                        staffRating={Math.round(item.staffRating / item.count * 10) / 10}
+                        adheranceRating={Math.round(item.adheranceRating / item.count * 10) / 10}
+                        extraData={item.extraData}
+                    />
                 </div>
-
-                <div className="rating">
-                    <span id="first">{(Math.floor(Math.round(((item.accumRating / 3) / item.count) * 10) / 10))}.</span><span id="last">{Math.round(((Math.round(((item.accumRating / 3) / item.count) * 10) / 10) - Math.floor(Math.round(((item.accumRating / 3) / item.count) * 10) / 10).toFixed()) * 10)}</span>
-                </div>
-
-                <div className="reviews">
-                    {item.count > 1 ? <span>{item.count} reviews</span> : <span>{item.count} review</span>}
-                </div>
-
-                <Comment
-                    cleanRating={Math.round(item.cleanRating / item.count * 10) / 10}
-                    staffRating={Math.round(item.staffRating / item.count * 10) / 10}
-                    adheranceRating={Math.round(item.adheranceRating / item.count * 10) / 10}
-                    extraData={item.extraData}
-                />
-            </div>
-        )
-    })
+            )
+        })
     // console.log(placeData)
 
     // const typeFilter = placeData.filter((item) => item.extraData.types[0] === 'bar')
