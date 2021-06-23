131b3a6b4b51c04eb67873e9147b2e9a960e6b88
Kevin Mitchell <kevinmitch14@gmail.com>
2021-06-23 12:57:18 +0200

    Redux implementation

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux'
+import store from './store'
+
+
 ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // <React.StrictMode>
   // </React.StrictMode>,
-  <App />,
   document.getElementById('root')
 );
 
