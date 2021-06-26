a83927ba2aeba3b90c7b14eb6304e85b8513e871
Kevin Mitchell <kevinmitch14@gmail.com>
2021-06-26 13:02:34 +0200

    Chip order changes

--- src/components/ChipHeader.js
+++ src/components/ChipHeader.js
@@ -5,10 +5,10 @@ const ChipHeader = () => {
     return (
         <div className='ChipHeader'>
             <ChipWrapper label="Restaurants" id={0} />
-            <ChipWrapper label="Hotels" id={1} />
-            <ChipWrapper label="Landmarks" id={2} />
-            <ChipWrapper label="Most Reviewed" id={3} />
             <ChipWrapper label="Bars" id={4} />
+            <ChipWrapper label="Most Reviewed" id={3} />
+            <ChipWrapper label="Landmarks" id={2} />
+            <ChipWrapper label="Hotels" id={1} />
             <ChipWrapper label="Retail" id={5} />
         </div>
     )
